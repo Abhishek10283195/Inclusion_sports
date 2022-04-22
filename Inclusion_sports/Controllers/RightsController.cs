288dc2951a0f693e69c6226f032dd75cf61c344e
anag0014 <anag0014@student.monash.edu>
2022-04-22 15:53:56 +1000

    Added 3 scenarios

---
+++ Inclusion_sports/Controllers/RightsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inclusion_sports.Controllers
+{
+    public class RightsController : Controller
+    {
+        // GET: Rights
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
