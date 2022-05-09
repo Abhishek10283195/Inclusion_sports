50a6a2a10d54c14251ab82b21f52baae43e50996
anag0014 <anag0014@student.monash.edu>
2022-05-09 15:06:31 +1000

    Added yoga code and video

---
+++ Inclusion_sports/Controllers/YogaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inclusion_sports.Controllers
+{
+    public class YogaController : Controller
+    {
+        // GET: Yoga
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
